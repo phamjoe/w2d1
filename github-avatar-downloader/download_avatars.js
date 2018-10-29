75565b63b7ed3838b710222f21920810c9f8eea1
Joseph Pham <j.pham017@gmail.com>
2018-10-29 16:10:21 -0400

    formatted code

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -1,16 +1,17 @@
-var request = require('request');
-var token = require('./secrets');
-var fs = require('fs');
-var input = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+const token = require('./secrets');
+
+const input = process.argv.slice(2);
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  const options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'request',
-      Authorization: token.GITHUB_TOKEN
-    }
+      Authorization: token.GITHUB_TOKEN,
+    },
   };
 
   request(options, function(err, res, body) {
@@ -18,35 +19,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-function downloadImageByURL(url, filePath) {
-  var bar = '|';
-
+function downloadImageByURL(url, filePath){
   request
     .get(url)
     .on('error', function(err) {
       throw err;
     })
-    .on('response', function(response) {})
-    .on('data', function() {
-      process.stdout.write(bar);
-    })
     .on('end', function() {
-      console.log('\nDownload Complete.');
+      console.log(`\nDownloading ${url}`);
     })
     .pipe(fs.createWriteStream(filePath));
-}
-if(input.length !== 0){
-getRepoContributors(input[0], input[1], function(err, result) {
-  let avatars = result;
-  console.log('Errors:', err);
-  for (let el of avatars) {
-    var filePath = `avatars/${el['login']}.jpg`;
-    fs.createWriteStream(filePath);
-    downloadImageByURL(el['avatar_url'], filePath);
-  }
-});
-}
 
-else{
+}
+// check if argument was passed
+if (input.length !== 0) {
+  getRepoContributors(input[0], input[1], function(err, result) {
+    const avatars = result;
+    console.log('Errors:', err);
+    for (let el of avatars) {
+      var filePath = `avatars/${el['login']}.jpg`;
+      fs.createWriteStream(filePath);
+      downloadImageByURL(el['avatar_url'], filePath);
+    }
+  });
+} else {
   throw 'Incorrect number of arguments. Provide repo owner and repo name';
 }
